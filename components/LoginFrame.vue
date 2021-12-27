afe4f49211e97a9069294a645f08c86511a4660b
mmikami-lifull <mikamimasahiro@lifull.com>
2021-12-27 14:09:23 +0900

    use vue-decolator and create basic components

---
+++ components/LoginFrame.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-card class="mx-auto" max-width="600">
+    <v-card-text class="primary--text">
+      LoginFrame
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class LoginFrame extends Vue {}
+</script>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,43 +1,12 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
+  <v-app>
+    <h1>Error</h1>
   </v-app>
 </template>
 
-<script>
-export default {
-  name: 'EmptyLayout',
-  layout: 'empty',
-  props: {
-    error: {
-      type: Object,
-      default: null,
-    },
-  },
-  data() {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
-    }
-  },
-  head() {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title,
-    }
-  },
-}
-</script>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
 
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
+@Component
+export default class ErrorLayout extends Vue {}
+</script>
